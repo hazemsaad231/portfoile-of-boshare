1a53261d138651de990055dfd474fb4256dcc0e5
Bu4ra-3mer <boshraamer30@gmail.com>
2026-01-12 00:12:55 +0200

    add some projects

--- src/components/Data/data.jsx
+++ src/components/Data/data.jsx
@@ -3,16 +3,21 @@ import { TbApi } from "react-icons/tb";
 import { FaDatabase, FaSave } from "react-icons/fa";
 import { MdArchitecture } from "react-icons/md";
 import { BiTransfer } from "react-icons/bi";
-import chat from '../../assets/images/chat.jpeg';
-import nots from '../../assets/images/nots.jpeg';
-import news from '../../assets/images/news.jpeg';
-import weather from '../../assets/images/weather.jpeg';
-import marsal from '../../assets/images/marsal.webp';
-import toku from '../../assets/images/toku.jpeg';
-import todo from '../../assets/images/todo.jpeg';
-import store from '../../assets/images/store.jpeg';
-import tunes from '../../assets/images/tunes.jpeg';
-import xo from '../../assets/images/xo.jpeg';
+import chat from "../../assets/images/chat.jpeg";
+import nots from "../../assets/images/nots.jpeg";
+import news from "../../assets/images/news.jpeg";
+import weather from "../../assets/images/weather.jpeg";
+import marsal from "../../assets/images/marsal.webp";
+import toku from "../../assets/images/toku.jpeg";
+import todo from "../../assets/images/todo.jpeg";
+import store from "../../assets/images/store.jpeg";
+import tunes from "../../assets/images/tunes.jpeg";
+import xo from "../../assets/images/xo.jpeg";
+import flash_light from "../../assets/images/flash_light.jpeg";
+import counter from "../../assets/images/counter.jpeg";
+import birht_day_card from "../../assets/images/birth_day_card.webp";
+import business_card from "../../assets/images/business_card.jpeg";
+import splash_screen from "../../assets/images/splash_screen.jpeg";
 
 
 
@@ -100,7 +105,7 @@ export const Flutter = [
         title: "Notes App",
         img:nots,
         link:'https://github.com/Bu4ra-3mer/notes_app',
-        skills: ["Flutter", "Clean Architecture", "Parse Server", "Freezed"],
+        skills: ["Flutter", "ListView.builder", "Hive","Cubit"],
         description: "A task management app built using Clean Architecture, integrating Parse Server for persistence and supporting Anonymous Sign-In."
     },
     {
@@ -108,7 +113,7 @@ export const Flutter = [
         title: "Chat App",
         img:chat,
         link:'https://github.com/Bu4ra-3mer/chat_app',
-        skills: ["Flutter", "Firebase Auth", "Cloud Firestore"],
+        skills: ["Flutter", "Firebase Auth", "Cloud Firestore","Navigation"],
         description: "A real-time chat application with secure user authentication and live messaging capabilities."
     },
     {
@@ -124,7 +129,7 @@ export const Flutter = [
         title: "Weather App",
         img:weather,
         link:'https://github.com/Bu4ra-3mer/weather_app',
-        skills: ["Flutter", "Dio", "RESTful API", "Cached Network Image"],
+        skills: ["Flutter", "http", "API Request", "Cached Network Image","Cubit"],
         description: "A weather app displaying real-time weather information using APIs and optimizing image loading."
     },
     {
@@ -132,15 +137,15 @@ export const Flutter = [
         title: "Toku App",
         img:toku,
         link:'https://meet.google.com/oef-adze-wfc',
-        skills: ["Flutter", "assets", "setstate", "audioplayers package"],
+        skills: ["Flutter", "SetState", "assets_audio_player","Local Data"],
         description: "Created a beginner-friendly Japanese learning app with audio-based interaction."
     },
       {
         id: 7,
-        title: "Todo App",
+        title: "Todo List",
         img:todo,
         link:'https://github.com/Bu4ra-3mer/todo_list.git',
-        skills: ["Flutter", "Freezed package ", "Clean Architecture ", "Parse server"],
+        skills: ["Flutter", "Freezed package ", "Clean Architecture ", "Parse Server","Handling Errors "],
         description: "To-Do app with Clean Architecture, Parse Server for storage, Freezed models, and Anonymous Sign-In."
     },
      {
@@ -148,7 +153,7 @@ export const Flutter = [
         title: "Store App",
         img:store,
         link:'https://github.com/Bu4ra-3mer/store_app.git',
-        skills: ["Flutter", "Handling errors ", "Reset APIs ", "Dio"],
+        skills: ["Flutter", "Handling Errors ", "Reset APIs", "Dio","GridView"],
         description: "A sleek and intuitive Store App to explore, manage, and update products effortlessly."
     },
     {
@@ -156,7 +161,7 @@ export const Flutter = [
         title: "Tunes player",
         img:tunes,
         link:'https://github.com/Bu4ra-3mer/tunes_player.git',
-        skills: ["Flutter", "Handling errors ", "setstate ", "audioplayers","List view"],
+        skills: ["Flutter", "SetState ", "audioplayers","Listview",],
         description: "A simple music player with seven note levels, demonstrating audio playback and user interaction."
     },
     {
@@ -164,9 +169,49 @@ export const Flutter = [
         title: "XO Game",
         img:xo,
         link:'https://github.com/Bu4ra-3mer/xo.git',
-        skills: ["Flutter", "Handling errors ", "setstate ", "Gridview"],
+        skills: ["Flutter", "Handling Errors", "Setstate", "GridView","GestureDetector"],
         description: "Classic XO game with score tracking for X and O players, providing a fun and competitive experience."
-    }
+    },
+    {
+        id: 11,
+        title: "Flash Light",
+        img:flash_light,
+        link:'https://github.com/Bu4ra-3mer/flash_light.git',
+        skills: ["Flutter", "Icons", "Setstate", ],
+        description: "Flashlight app with yellow and gray toggle logo."
+    },
+    {
+        id: 12,
+        title: "Counter",
+        img:counter,
+        link:'https://github.com/Bu4ra-3mer/count.git',
+        skills: ["Flutter", "Containers", "Setstate", ],
+        description: "A simple counter app demonstrating state management using setState, perfect for tracking increments and decrements."
+    },
+    {
+        id: 13,
+        title: "Business Card",
+        img:business_card,
+        link:'https://github.com/Bu4ra-3mer/business_card.git',
+        skills: ["Flutter", "ListTail",  "asset", "UI Design" ],
+        description: "An interactive digital business card app to showcase contact information in a sleek, mobile-friendly design."
+    },
+    {
+        id: 14,
+        title: "Birthday Card",
+        img:birht_day_card,
+        link:'https://github.com/Bu4ra-3mer/birth_day_card.git',
+        skills: ["Flutter", "asset","UI Design" ],
+        description: "A creative birthday card app to send personalized wishes and greetings digitally."
+    },
+    {
+        id: 15,
+        title: "Splash Screen",
+        img:splash_screen,
+        link:'https://github.com/Bu4ra-3mer/splash_screen.git',
+        skills: ["Flutter", "RichText","UI Design" ],
+        description: "A colorful splash screen app using RichText to display each word in a different color."
+    },
 ];
 
 export const experience = [
